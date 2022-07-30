571b71f6c41fe93cf66f127fee83d2b825829f25
antaltettinger <a.tettinger@uniandes.edu.co>
2022-07-30 19:43:02 +0200

    Build setup

---
+++ src/components/EmployeeBoard/EmployeeBoard.test.tsx
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest'
+import { render, screen } from '../../utils/test-utils'
+import EmployeeBoard from './EmployeeBoard';
+
+describe('Simple working test', () => {
+  it('the title is visible', () => {
+    render(<EmployeeBoard />)
+    expect(screen.getByText(/This is the employee board/i)).toBeInTheDocument()
+  })
+})
---
+++ src/components/EmployeeBoard/EmployeeBoard.tsx
@@ -0,0 +1,7 @@
+const EmployeeBoard = () => (
+  <>
+    <h1>This is the employee board</h1>
+  </>
+);
+
+export default EmployeeBoard
