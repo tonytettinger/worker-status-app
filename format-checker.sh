2134bed48a4c8d79c3080c86783b48bdc7e019da
antaltettinger <a.tettinger@uniandes.edu.co>
2022-07-30 17:19:43 +0200

    build:commit message check added

---
+++ format-checker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+INPUT_FILE=$1
+START_LINE=`head -n1 $INPUT_FILE`
+PATTERN="^(?<type>build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test|¯\\_\(ツ\)_\/¯)(?<scope>\(\w+\)?((?=:\s)|(?=!:\s)))?(?<breaking>!)?(?<subject>:\s.*)?|^(?<merge>Merge \w+)"
+if ! [[ "$START_LINE" =~ $PATTERN ]]; then
+  echo "Unsupported format, see example: Jira-123: Hello World"
+  exit 1
+fi
