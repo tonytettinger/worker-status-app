9b5ceeb3e87e158fd6a003e3a83437e1d7659601
antaltettinger <a.tettinger@uniandes.edu.co>
2022-07-31 18:44:37 +0200

    refactor: common css extracted

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import './common/styles';
 import EmployeeBoard from './components/EmployeeBoard/EmployeeBoard';
 
-const App = () => <EmployeeBoard />;
+const App: React.FC = () => <EmployeeBoard />;
 
 export default App;
